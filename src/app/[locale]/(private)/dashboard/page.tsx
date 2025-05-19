a5005ae54b6fb6a3f568fcf44d3e5d45572e9310
RanTsai <mycophyte5164@gmail.com>
2025-05-19 13:42:53 +0200

    Basic test completed to start build frame

--- src/app/[locale]/(private)/dashboard/page.tsx
+++ src/app/[locale]/(private)/dashboard/page.tsx
@@ -1,24 +1,10 @@
 "use server";
 import React from 'react';
 
-import { currentUser } from '@clerk/nextjs/server';
-import userGlobalStore , { IUserGlobalStore } from '@/app/global-store/users-store';
 import UserDashboard from './userdashboard';
 import Userfeedback from '@/components/userfeedback';
 
 export default async function Page() {
-  //const { user } = userGlobalStore() as IUserGlobalStore;
-
-  //const user = await currentUser();
-
-  // const plainUser = user
-  //   ? {
-  //     id: user.id,
-  //     email: user.emailAddresses?.[0]?.emailAddress || null,
-  //     username: user.username,
-  //     imageUrl: user.imageUrl,
-  //   }
-  //   : null;
 
   return (
     <div>
